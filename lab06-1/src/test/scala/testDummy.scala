e9683cdad5329c1f66d239854081742eb6a1eafc
elyoun <jsoares.uni@gmail.com>
2022-04-10 20:22:26 +0100

    Worksheet para Dummy

--- lab06-1/src/test/scala/testDummy.scala
+++ lab06-1/src/test/scala/testDummy.scala
@@ -7,3 +7,10 @@ class testDummy extends AnyFunSuite:
     val expected = 7
     assert(Dummy.stringToInt(input) === expected)
   }
+
+  test("combineLists1"){
+    val input = (List(18.7,16.3), List(11.5,12.1), List(18.1,19.9))
+    val input2 = ("sum")
+    val expected = List(48.3, 48.3)
+    assert(Dummy.combineLists(input) (input2) === expected)
+  }
